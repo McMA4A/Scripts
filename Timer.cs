140956e8a99c412536b8664e697d2603dd329b29
McMA4A <152397855+McMA4A@users.noreply.github.com>
2023-12-22 17:32:53 +0400

    Add files via upload

--- Timer.cs
+++ Timer.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
+    public TextMeshProUGUI timer;
     public int minutes = 3;
     public float secundes = 59.0f;
     void Update()
     {
         secundes -= Time.deltaTime;
+        timer.text = minutes + ":" + secundes;
         if (secundes <= 0)
         {
             if (minutes > 0)
